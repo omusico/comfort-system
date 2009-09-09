833ec0c387d0cd9d800a4d8fc28bd1bcbee60456
mormih@gmail.com <mormih@gmail.com@5fa5832e-9d21-11de-a819-4b28d33f9070>
2009-09-09 10:08:24 +0000

    test svn

--- trunk/web-beans/src/comfort/web/beans/Koko.java
+++ trunk/web-beans/src/comfort/web/beans/Koko.java
@@ -3,13 +3,6 @@ package comfort.web.beans;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.In;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Миша
- * Date: 08.04.2009
- * Time: 2:17:55
- * To change this template use File | Settings | File Templates.
- */
 @Name("koko")
 public class Koko {
     @In
